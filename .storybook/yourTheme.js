be36cb23bfda91a91646fb30e926dbcc1819f87d
aaho <adam.aho@arcticwolf.com>
2019-12-03 14:55:09 -0500

    adding some cleanup

--- .storybook/yourTheme.js
+++ .storybook/yourTheme.js
@@ -2,6 +2,6 @@ import { create } from '@storybook/theming';
 
 export default create({
   base: 'light',
-  brandTitle: 'Arctic Wolf Design',
+  brandTitle: 'Arctic Wolf UI',
   fontBase: "'Encode Sans', sans-serif"
 });
--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-Yet To Be Named Wolfie
-======================
-
-![](https://github.com/rtkwolf/ui_components/workflows/tests/badge.svg)
+Arctic Wolf UI
+==============
 
