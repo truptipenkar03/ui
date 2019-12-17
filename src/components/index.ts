c6517f5a849667a9f227aa001fa2494a2dfcc855
rtkaaho <adam.aho@arcticwolf.com>
2019-12-17 13:43:55 -0500

    export all of the components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,13 @@
+import { Accordion } from "./accordion/Accordion";
 import { Button } from './button/Button';
 import { Collapse } from './collapse/Collapse';
+import { Input } from "./input/Input";
+import { Panel } from "./panel/Panel";
 
 export {
+  Accordion,
   Button,
-  Collapse
+  Collapse,
+  Input,
+  Panel
 };
