46a9d074c489cbaf1575d63bb165b1811f85d9d1
rtkaaho <adam.aho@arcticwolf.com>
2020-01-07 12:11:54 -0500

    adding some docs for portal

--- src/components/portal/stories/Portal.stories.tsx
+++ src/components/portal/stories/Portal.stories.tsx
@@ -1,10 +1,6 @@
 import * as React from 'react';
 
 import {
-  storiesOf
-} from '@storybook/react';
-
-import {
   motion,
   AnimatePresence
 } from "framer-motion";
@@ -13,79 +9,49 @@ import {
   Button
 } from '../..';
 
+// @ts-ignore
+import mdx from './Portal.mdx';
+
 import {
   Portal
 } from '../Portal';
 
-const stories = storiesOf('Portal', module);
-
-stories.add(
-  'Default',
-  () => {
-    const [showPortal, setShowPortal] = React.useState(false);
-    return (
-      <React.Fragment>
-        <Button onClick={() => setShowPortal(!showPortal)}>
-          Show Portal
-        </Button>
-        <AnimatePresence>
-          {showPortal &&
-            <Portal>
-              <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-              >
-                <div>I am the portal</div>
-              </motion.div>
-            </Portal>
-          }
-        </AnimatePresence>
-        <Portal>
-          <div style={{ visibility: 'hidden' }}>
-            <div>I am the portal</div>
-          </div>
-        </Portal>
-      </React.Fragment>
-    );
+export default {
+  title: 'Components/Portal',
+  component: Portal,
+  parameters: {
+    docs: {
+      page: mdx
+    },
   },
-  { info: { propTablesExclude: [Button] },
-  }
-);
+};
 
-// stories.add(
-//   'Custom Mount Node',
-//   () => {
-//     const [showPortal, setShowPortal] = React.useState(false);
-//     const container = React.useRef(null);
-//
-//     return (
-//       <React.Fragment>
-//         <Button onClick={() => setShowPortal(!showPortal)}>
-//           Show Portal
-//         </Button>
-//         <AnimatePresence>
-//           {showPortal &&
-//           <Portal container={container.current}>
-//             <motion.div
-//               initial={{ opacity: 0 }}
-//               animate={{ opacity: 1 }}
-//               exit={{ opacity: 0 }}
-//             >
-//               <div>I am the portal</div>
-//             </motion.div>
-//           </Portal>
-//           }
-//         </AnimatePresence>
-//         <div className="container-to-mount-to" ref={container} />
-//           <Portal>
-//             <div style={{ visibility: 'hidden' }}>
-//               <div>I am the portal</div>
-//             </div>
-//           </Portal>
-//       </React.Fragment>
-//     );
-//   },
-//   { info: { propTablesExclude: [Button] },
-//   }
-// );
+
+export const simple = () => {
+  const [showPortal, setShowPortal] = React.useState(false);
+  return (
+    <React.Fragment>
+      <Button onClick={() => setShowPortal(!showPortal)}>
+        Show Portal
+      </Button>
+      <AnimatePresence>
+        {showPortal &&
+        <Portal>
+          <motion.div
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+          >
+            <div>I am the portal</div>
+          </motion.div>
+        </Portal>
+        }
+      </AnimatePresence>
+      <Portal>
+        <div style={{visibility: 'hidden'}}>
+          <div>I am the portal</div>
+        </div>
+      </Portal>
+    </React.Fragment>
+  );
+};
