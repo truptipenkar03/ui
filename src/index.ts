1c769522729e9de1213af1d477981a17c58b5944
rtkaaho <adam.aho@arcticwolf.com>
2020-01-17 11:47:04 -0500

    uncommenting the rest

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-// export {
-//   Accordion,
-//   AccordionProps,
-//   AccordionFunctionComponent
-// } from './components/accordion/Accordion';
-//
-// export {
-//   ItemGapType,
-//   ItemKeyType,
-//   ExpandedItemsType
-// } from './components/accordion/types';
+export {
+  Accordion,
+  AccordionProps,
+  AccordionFunctionComponent
+} from './components/accordion/Accordion';
+
+export {
+  ItemGapType,
+  ItemKeyType,
+  ExpandedItemsType
+} from './components/accordion/types';
 
 export {
   Button,
@@ -18,63 +18,63 @@ export {
   SizeType
 } from './components/button/Button';
 
-// export {
-//   Collapse,
-//   CollapseProps
-// } from './components/collapse/Collapse';
-//
-// export {
-//   Floater,
-//   FloaterProps
-// } from './components/floater/Floater';
-//
-// export {
-//   Icon
-// } from './components/icons';
-//
-// export {
-//   Input,
-//   InputProps,
-//   BorderType,
-//   InputSize,
-//   ValidationStatus
-// } from './components/input/Input';
-//
-// export {
-//   Modal,
-//   ModalProps
-// } from './components/modal/Modal';
-//
-// export {
-//   Panel,
-//   PanelProps
-// } from './components/panel/Panel';
-//
-// export {
-//   Portal,
-//   PortalProps
-// } from './components/portal/Portal';
-//
-// export {
-//   useAfterMountEffect,
-//   useTheme
-// } from './hooks';
-//
-// export {
-//   getRelativePosition,
-// } from './utils';
-//
-// export {
-//   Position
-// } from './utils/getRelativePosition';
-//
-// export {
-//   createTheme
-// } from './theme';
-//
-// export {
-//   GlobalTheme,
-//   Colors
-// } from './theme/types';
-//
-// export * from './styled';
+export {
+  Collapse,
+  CollapseProps
+} from './components/collapse/Collapse';
+
+export {
+  Floater,
+  FloaterProps
+} from './components/floater/Floater';
+
+export {
+  Icon
+} from './components/icons';
+
+export {
+  Input,
+  InputProps,
+  BorderType,
+  InputSize,
+  ValidationStatus
+} from './components/input/Input';
+
+export {
+  Modal,
+  ModalProps
+} from './components/modal/Modal';
+
+export {
+  Panel,
+  PanelProps
+} from './components/panel/Panel';
+
+export {
+  Portal,
+  PortalProps
+} from './components/portal/Portal';
+
+export {
+  useAfterMountEffect,
+  useTheme
+} from './hooks';
+
+export {
+  getRelativePosition,
+} from './utils';
+
+export {
+  Position
+} from './utils/getRelativePosition';
+
+export {
+  createTheme
+} from './theme';
+
+export {
+  GlobalTheme,
+  Colors
+} from './theme/types';
+
+export * from './styled';
