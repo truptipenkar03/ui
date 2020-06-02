4c49295eca4541d9e156dc1ecb17ed2512e9ff2e
Adam Aho <56700218+rtkaaho@users.noreply.github.com>
2020-06-02 13:08:15 -0400

    0.2.5 bump (#125)

--- docs/main.699ccdf13c9bb7e8629c.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{1248:function(n,o,c){"use strict";c.r(o);c(1249)},453:function(n,o,c){c(454),c(597),c(1248),n.exports=c(1356)},516:function(n,o){}},[[453,1,2]]]);
